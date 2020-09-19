8afc54c599904ad4267f75e42efb4007061c89db
Ray Azrin Karim <rayazrin19@gmail.com>
2020-09-19 22:06:03 +0700

    finish simple hello world project

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.kt
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.mobileprogramming.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.button)
+
+        btnClickMe.setOnClickListener {
+            // make a toast on button click event
+            Toast.makeText(this, "Downloaded 4GB RAM", Toast.LENGTH_LONG).show()
+        }
+    }
+}
