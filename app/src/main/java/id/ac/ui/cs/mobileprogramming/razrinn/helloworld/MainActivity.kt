cc5d687c74d67e81333f055e268d003acee0110a
Ray Azrin Karim <rayazrin19@gmail.com>
2020-09-24 20:56:34 +0700

    Refactor package to include my name

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.mobileprogramming.helloworld
+package id.ac.ui.cs.mobileprogramming.razrinn.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/ExampleUnitTest.kt
+++ app/src/test/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.mobileprogramming.helloworld
+package id.ac.ui.cs.mobileprogramming.razrinn.helloworld
 
 import org.junit.Test
 
