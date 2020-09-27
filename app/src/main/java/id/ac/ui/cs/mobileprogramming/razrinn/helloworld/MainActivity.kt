b07471f353a6456de0d0302c607390c5eae0e08a
Ray Azrin Karim <rayazrin19@gmail.com>
2020-09-27 19:08:45 +0700

    add counter button

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
@@ -1,20 +1,39 @@
 package id.ac.ui.cs.mobileprogramming.razrinn.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClickMe = findViewById<Button>(R.id.button)
+        val downloadRamButton = findViewById<Button>(R.id.button)
 
-        btnClickMe.setOnClickListener {
+        downloadRamButton.setOnClickListener {
             // make a toast on button click event
             Toast.makeText(this, "Downloaded 4GB RAM", Toast.LENGTH_LONG).show()
         }
+
+        val addCounterButton = findViewById<Button>(R.id.button2)
+        val counterValue = findViewById<TextView>(R.id.counter)
+        counterValue.text = """$counter"""
+        addCounterButton.setOnClickListener{
+            counterValue.text = """${addCounter(counter)}"""
+            this.counter += 1
+        }
+        
+        
+    }
+
+    fun addCounter(counter: Int): Int {
+        return counter + 1
     }
 }
