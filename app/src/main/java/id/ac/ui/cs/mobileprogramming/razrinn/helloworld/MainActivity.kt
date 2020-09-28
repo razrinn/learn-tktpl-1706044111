5b44ef40386d87b6bb813274c680a9fbb0394272
Ray Azrin Karim <rayazrin19@gmail.com>
2020-09-28 18:54:00 +0700

    remove spacing

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razrinn/helloworld/MainActivity.kt
@@ -8,20 +8,16 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     private var counter = 0
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val downloadRamButton = findViewById<Button>(R.id.button)
-
         downloadRamButton.setOnClickListener {
             // make a toast on button click event
             Toast.makeText(this, "Downloaded 4GB RAM", Toast.LENGTH_LONG).show()
         }
-
         val addCounterButton = findViewById<Button>(R.id.button2)
         val counterValue = findViewById<TextView>(R.id.counter)
         counterValue.text = """$counter"""
@@ -29,10 +25,7 @@ class MainActivity : AppCompatActivity() {
             counterValue.text = """${addCounter(counter)}"""
             this.counter += 1
         }
-        
-        
     }
-
     fun addCounter(counter: Int): Int {
         return counter + 1
     }
