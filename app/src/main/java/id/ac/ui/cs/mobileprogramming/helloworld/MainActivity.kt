d93b1bbb9471d88ec9aad6fe35318ac22db336db
Ray Azrin Karim <rayazrin19@gmail.com>
2020-10-05 00:03:31 +0700

    finish timer functionality

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.mobileprogramming.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -12,12 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val btnClickMe = findViewById<Button>(R.id.button)
-
         btnClickMe.setOnClickListener {
             // make a toast on button click event
             Toast.makeText(this, "Downloaded 4GB RAM", Toast.LENGTH_LONG).show()
+        }
+        val btnClickTimerActivity = findViewById<Button>(R.id.button2)
+        btnClickTimerActivity.setOnClickListener {
+            navigateToTimerActivity()
         }
     }
 
@@ -29,5 +32,11 @@ class MainActivity : AppCompatActivity() {
         this.doubleBackToExitPressedOnce = true
         Toast.makeText(this, "Back again to exit", Toast.LENGTH_SHORT).show()
         Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    }
+
+    private fun navigateToTimerActivity(){
+        val intent = Intent(this, TimerActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
