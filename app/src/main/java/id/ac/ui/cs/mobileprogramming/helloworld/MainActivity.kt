51ee2c45d07c8af47768ead2f087b330878868fa
Ray Azrin Karim <rayazrin19@gmail.com>
2020-10-04 21:21:47 +0700

    add handler if back is pressed

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.kt
@@ -2,10 +2,13 @@ package id.ac.ui.cs.mobileprogramming.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var doubleBackToExitPressedOnce = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             // make a toast on button click event
             Toast.makeText(this, "Downloaded 4GB RAM", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Back again to exit", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
     }
 }
